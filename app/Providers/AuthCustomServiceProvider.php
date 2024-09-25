6d012045aa78033475cf83d2c82dd1ba42d1ab7d
sidy Diop Balde <sididiop53@gmail.com>
2024-09-25 20:48:14 +0000

    commit

--- app/Providers/AuthCustomServiceProvider.php
+++ app/Providers/AuthCustomServiceProvider.php
@@ -16,15 +16,15 @@ class AuthCustomServiceProvider extends ServiceProvider
         $this->app->singleton(AuthentificationServiceInterface::class, function ($app) {
             return new AuthentificationPassport();
         });
-        $this->app->singleton(FirebaseAuth::class, function ($app) {
-            return (new Factory)
-                ->withServiceAccount([
-                    'project_id' => env('FIREBASE_PROJECT_ID'),
-                    'client_email' => env('FIREBASE_CLIENT_MAIL'),
-                    'private_key' => env('FIREBASE_PRIVATE_KEY'),
-                ])
-                ->createAuth();
-        });
+        // $this->app->singleton(FirebaseAuth::class, function ($app) {
+        //     return (new Factory)
+        //         ->withServiceAccount([
+        //             'project_id' => env('FIREBASE_PROJECT_ID'),
+        //             'client_email' => env('FIREBASE_CLIENT_MAIL'),
+        //             'private_key' => env('FIREBASE_PRIVATE_KEY'),
+        //         ])
+        //         ->createAuth();
+        // });
 
       
         $this->app->singleton(AuthentificationFirebase::class, function ($app) {
